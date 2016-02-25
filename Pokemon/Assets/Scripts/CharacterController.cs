30a3c38bac578d2a76070be3b97ac746e87e1266
Milton van de Sanden <milton.vandesanden@gmail.com>
2016-02-25 17:42:45 +0100

    stuff

---
+++ Pokemon/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour
+{
+    public Rigidbody rigidbody;
+	
+    // Use this for initialization
+	void Start ()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float moveHorizontal = Input.GetButtonDown;
+	}
+}
